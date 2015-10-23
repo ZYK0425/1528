3f93c9bfd28d49aac944576c07435d841cafd208
赵英奎 <zhaoyingkui@1.local>
2015-10-23 15:17:11 +0800

    play

---
+++ GitRemoteClass/GitRemoteClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitRemoteClass
+//
+//  Created by 赵英奎 on 15/10/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+-(void)play;
+
+@end
+
